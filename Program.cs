b75ba20b9f7d455ef44fd7c7680754fd95c89f47
markspooner89 <markspooner89@outlook.com>
2024-12-11 18:48:35 +0000

    add error pages

--- Program.cs
+++ Program.cs
@@ -1,19 +1,22 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
-    // app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseStatusCodePagesWithReExecute("/Errors/{0}");
+    app.UseExceptionHandler("/Errors/500");
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+
 app.UseStaticFiles();
 
 app.UseRouting();
